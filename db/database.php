9578d0b063f30d9d00e1189edeaae5616d698126
Shaoun Chandra Shill <shaounchandrashill75@gmail.com>
2025-06-08 18:20:08 +0600

    ✔️Duplicate Database Eroor Fix

--- db/database.php
+++ db/database.php
@@ -3,6 +3,9 @@
 function todo_module_create_table() {
     global $wpdb;
     $table_name = $wpdb->prefix . 'todo_tasks';
+
+    // Check if table exists
+    if ($wpdb->get_var("SHOW TABLES LIKE '$table_name'") != $table_name) {
     $charset_collate = $wpdb->get_charset_collate();
 
     $sql = "CREATE TABLE $table_name (
@@ -15,4 +18,6 @@ function todo_module_create_table() {
 
     require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
     dbDelta($sql);
+    
+    }
 }
